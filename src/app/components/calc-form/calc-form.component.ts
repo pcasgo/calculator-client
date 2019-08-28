75e80176789b3e5a9e2c2cab55bb1c75d5a8fd84
Pablo <pablo.castro@witi.cl>
2019-08-27 22:05:33 -0400

    [FIX]: Creacion de metodo para separar opersciones y calcular de forms individual

--- src/app/components/calc-form/calc-form.component.ts
+++ src/app/components/calc-form/calc-form.component.ts
@@ -12,7 +12,10 @@ export class CalcFormComponent implements OnInit {
   keys: string[] = [];
   result = '';
   numbersDto: NumbersI[];
-  json = '';
+  primeraParteCadena = 0;
+  segundaParteCadena = 0;
+  cadena = '12+768/7*6*7+5/2/4+8';
+  reGex = new RegExp('[+/*-]', 'gim');
   constructor(private calcFormService: CalcFormService,
   ) { }
 
@@ -36,12 +39,7 @@ export class CalcFormComponent implements OnInit {
 
   calculate() {
     this.sendData(this.keys);
-    // Procedimiento
-    // 3 - 4 * 5 + 6 / 5 * 8
-    // 3 - 20 + 6 / 5 * 8
-    // 3 - 20 + 1.2 *8
-    // 3- 20 + 9.6
-    // 3 -20 + 9.6
+    this.test(this.result);
   }
 
   sendData(numbers) {
@@ -111,5 +109,83 @@ export class CalcFormComponent implements OnInit {
 
   reverseChain(chain) {
     return chain.split('').reverse().join('');
+  }
+
+  test(cadena) {
+    console.log('cadena: ', cadena);
+    if (cadena.match(this.reGex)) {
+      return true;
+    } else {
+      // let cadena = '12+768/7*6*7+5/2/4+8';
+      // tomo todos los valores anteriores a mult o div y todos los posteriores y resuelvo
+      let newCadena = '';
+      let newValue = '';
+      let operation = '';
+      for (let i = 0; i <= this.cadena.length - 1; i++) {
+        if (this.cadena[i] === '/' || this.cadena[i] === '*') {
+          operation = this.cadena[i];
+          // Busco la primera operacion que sea de tipo: [/ o *]
+          console.log('aqui: ', this.cadena.indexOf(this.cadena[i]));
+          const dto = new NumbersI();
+          dto.numberOne = this.obtienePrimerNumeroOperacion(this.cadena, this.cadena[i]);
+          dto.numberTwo = this.obtieneSegundoNumeroOperacion(this.cadena, this.cadena[i]);
+          // tslint:disable-next-line: no-eval
+          console.log('eval: ', eval(dto.numberOne + operation + dto.numberTwo));
+          // tslint:disable-next-line: no-eval
+          newValue = eval(dto.numberOne + operation + dto.numberTwo).toFixed(2);
+          // newValue = this.calculateOperation(dto, operation);
+          newCadena = this.actualizaCadena(this.primeraParteCadena, this.segundaParteCadena, newValue);
+          console.log('newCadena: ', newCadena);
+          // Al encontrar la primera / o * detengo el bucle
+          break;
+        } else {
+          // Aqui solo hay sumas y restas
+        }
+      }
+      return this.test(newCadena);
+    }
+  }
+
+  obtienePrimerNumeroOperacion(cadena, indice) {
+    let one = '';
+    for (let i = cadena.indexOf(indice) - 1; i >= 0; i--) {
+      // Si no es numero corto el recorrido y guardo la primera parte de la operacion
+      if (isNaN(cadena[i])) {
+        this.primeraParteCadena = i;
+        break;
+      } else {
+        // Si es numero voy guardando como parte de la operacion
+        one += cadena[i];
+      }
+    }
+    one = this.reverseChain(one);
+    return one;
+  }
+
+  obtieneSegundoNumeroOperacion(cadena, indice) {
+    let two = '';
+    for (let i = cadena.indexOf(indice) + 1; i <= cadena.length - 1; i++) {
+      // Guardo numeros siguientes de la operacion
+      if (isNaN(cadena[i])) {
+        this.segundaParteCadena = i;
+        break;
+      } else {
+        // Si es numero voy guardando como parte de la operacion
+        two += cadena[i];
+      }
+    }
+    return two;
+  }
+
+  actualizaCadena(indiceInicio, indiceFin, newValue) {
+    let leftPart = '';
+    let rightPart = '';
+    for (let i = 0; i <= indiceInicio; i++) {
+      leftPart += this.cadena[i];
+    }
+    for (let i = indiceFin; i <= this.cadena.length - 1; i++) {
+      rightPart += this.cadena[i];
+    }
+    return leftPart + newValue + rightPart;
   }
 }
